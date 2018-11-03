e28b70b6344a8100993fb06993f8ca2e438014ec
DatPham123 <d.pham2013@yahoo.com>
2018-11-03 15:25:54 -0500

    regular logout

--- app/src/main/java/edu/uco/dpham9/datprobertmtermproject/AddTraineeExercise.kt
+++ app/src/main/java/edu/uco/dpham9/datprobertmtermproject/AddTraineeExercise.kt
@@ -1,12 +1,39 @@
 package edu.uco.dpham9.datprobertmtermproject
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import kotlinx.android.synthetic.main.activity_add_trainee_exercise.*
 
 class AddTraineeExercise : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_trainee_exercise)
+
+        id_trainee_find_vidBtn.setOnClickListener {
+            val openVid = Intent(Intent.ACTION_PICK)
+
+            val externalStorageDirectory = Environment.
+                getExternalStorageDirectory()
+
+            val pathName = externalStorageDirectory.path
+
+            val data = Uri.parse(pathName)
+
+            openVid.setDataAndType(data, "video/*")
+            startActivity(openVid)
+        }
     }
+
+
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+//        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+//            val videoUri: Uri = intent.data
+//            mVideoView.setVideoURI(videoUri)
+//        }
+//    }
 }
